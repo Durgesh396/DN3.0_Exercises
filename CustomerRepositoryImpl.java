5acf53ee768b1362c90d14ff4d33f746e43ec475
Durgesh396 <turnergnanavelu@gmail.com>
2024-07-30 14:34:12 +0530

    Add files via upload

---
+++ CustomerRepositoryImpl.java
@@ -0,0 +1,14 @@
+package DigitalNuture;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+	@Override
+	    public String findCustomerById(int id) {
+	        // Simulate fetching customer from a database
+	        return "Customer with ID: " + id;
+	    }
+	
+
+	}
+
+
